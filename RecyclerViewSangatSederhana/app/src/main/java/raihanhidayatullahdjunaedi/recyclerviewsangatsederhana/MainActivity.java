de81ed9502ffe4bd7dcdcd5c7df96b1ba96995af
raihanhd12 <fashfour487@gmail.com>
2022-09-20 11:54:56 +0700

    Raihan Ganteng Commit Pertama

---
+++ RecyclerViewSangatSederhana/app/src/main/java/raihanhidayatullahdjunaedi/recyclerviewsangatsederhana/MainActivity.java
@@ -0,0 +1,40 @@
+package raihanhidayatullahdjunaedi.recyclerviewsangatsederhana;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import raihanhidayatullahdjunaedi.recyclerviewsangatsederhana.adapters.SuperHeroAdapter;
+import raihanhidayatullahdjunaedi.recyclerviewsangatsederhana.models.SuperHero;
+
+public class MainActivity extends AppCompatActivity {
+    //instansiasi Recyclerview
+    RecyclerView rvSuperHero;
+    //instansiasi list superhero
+    List<SuperHero> listSuperHero = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //menyambungkan rvSuperHero ke layout
+        rvSuperHero = findViewById(R.id.rvSuperHero);
+        //Membuat object hero
+        SuperHero hero = new SuperHero("Petruk");
+        //menambahkan hero ke listSuperHero
+        listSuperHero.add(hero);
+        //membuat object hero baru
+        hero = new SuperHero("Gareng");
+        //menambahkan hero baru ke listSuperhero
+        listSuperHero.add(hero);
+        //Instansiasi Adapter
+        SuperHeroAdapter superHeroAdapter = new SuperHeroAdapter(listSuperHero);
+        //set adapter dan layoutmanager
+        rvSuperHero.setAdapter(superHeroAdapter);
+        rvSuperHero.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
---
+++ RecyclerViewSangatSederhana/app/src/main/java/raihanhidayatullahdjunaedi/recyclerviewsangatsederhana/models/SuperHero.java
@@ -0,0 +1,17 @@
+package raihanhidayatullahdjunaedi.recyclerviewsangatsederhana.models;
+
+public class SuperHero {
+    public String heroName;
+
+    public SuperHero(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+}
